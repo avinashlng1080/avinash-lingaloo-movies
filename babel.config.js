9096f7cfb7c3a1e3c17469e6d2db5a3406d7c587
Elias Nahum <nahumhbl@gmail.com>
2020-07-08 13:28:37 -0400

    Babel and TS config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                root: ['.'],
+                extensions: ['.tsx', '.ts', '.js'],
+                alias: {
+                    '@app/types': './app/types',
+                    '@assets': './app/assets',
+                    '@components': './app/components',
+                    '@data': './app/data',
+                    '@navigation': './app/navigation',
+                    '@screens': './app/screens',
+                    '@utils': './app/utils',
+                },
+            },
+        ],
+    ],
 };
