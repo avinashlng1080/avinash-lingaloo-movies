700ad55764783627f2c99cce030b09cc3c60868c
Avinash Lingaloo <avinashl@wigroupinternational.com>
2020-08-27 23:00:22 +0400

    Axios Retry working with 100% lost network config

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Mattermost - Avinash Lingaloo
 
 1. We are using a Firebase Cloud function to retrieve the generated movies based on user inputs.
+2. To test for network resilience, please use DSL mode on 'Network Link Conditioner'
--- app/screens/Start.tsx
+++ app/screens/Start.tsx
@@ -4,14 +4,13 @@ import {SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
 import {useValue} from 'react-native-redash';
 import axios from 'axios';
 import Movie from '@components/Movie';
-
+import * as rax from 'retry-axios';
 import type MovieType from '@app/types/Movie';
 import {MOVIE_POSTER} from '@utils/CONSTANTS';
 import List from '@components/List';
 import SpinnerModal from '@components/SpinnerModal';
 import MovieDetail from '@components/MovieDetail';
 
-// TODO : show processing spinner when fetching for movies
 // TODO : react-navigation-shared-element as per William Candillon
 
 const Start = () => {
@@ -27,7 +26,6 @@ const Start = () => {
 
     useFocusEffect(
         React.useCallback(() => {
-            console.log('in useFocusEffect');
             activeMovieId.setValue(-1);
         }, [activeMovieId]),
     );
@@ -40,6 +38,8 @@ const Start = () => {
     const getMovies = async () => {
         try {
             setFetching(true);
+            const interceptorId = rax.attach();
+            console.log('attache', interceptorId);
             const movieResponse = await axios({
                 method: 'post',
                 url:
@@ -49,6 +49,33 @@ const Start = () => {
                     reviewsPerMovie: 1,
                 },
                 headers: {'Content-Type': 'application/json'},
+                raxConfig: {
+                    // Retry 3 times on requests that return a response (500, etc) before giving up.
+                    retry: 3,
+                    // Retry twice on errors that don't return a response (ENOTFOUND, ETIMEDOUT, etc).
+                    noResponseRetries: 1,
+                    retryDelay: 100,
+
+                    // Defaults to:['GET', 'HEAD', 'OPTIONS', 'DELETE', 'PUT']
+                    httpMethodsToRetry: ['POST'], // NOTE:  COMMENT THIS LINE TO SEE IT CRASHING AT 500 error code <<<<
+
+                    // The response status codes to retry.
+                    statusCodesToRetry: [
+                        [100, 199],
+                        [429, 429],
+                        [500, 599],
+                    ],
+
+                    // You can set the backoff type.
+                    // options are 'exponential' (default), 'static' or 'linear'
+                    backoffType: 'exponential',
+                    // You can detect when a retry is happening, and figure out how many
+                    // retry attempts have been made
+                    onRetryAttempt: (err) => {
+                        const cfg = rax.getConfig(err);
+                        console.log('Retry attempt <<<<<<<<<<<<<<');
+                    },
+                },
             });
             const movie = movieResponse?.data?.movies;
             setMovies(movie);
